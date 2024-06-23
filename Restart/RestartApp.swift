cd4242b165adfbcf16e4db2bbb34a7c4c8eab2bc
Douglas Silva <douglas.edig@outlook.com>
2024-06-22 23:58:04 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Douglas Matheus da Silva on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
