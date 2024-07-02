2d4a772812a36693c9cc7d734ceb42b91854ec1a
Douglas Silva <douglas.edig@outlook.com>
2024-07-02 00:16:08 -0300

    feat: finish animation part one

--- Restart/Views/CircleGroupView.swift
+++ Restart/Views/CircleGroupView.swift
@@ -11,6 +11,7 @@ struct CircleGroupView: View {
   
   @State var ShapeColor: Color
   @State var ShapeOpacity: Double
+  @State private var isAnimation: Bool = false
   
   
     var body: some View {
@@ -22,6 +23,13 @@ struct CircleGroupView: View {
           .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
           .frame(width: 260, height: 260, alignment: .center)
       }//: zstack
+      .blur(radius: isAnimation ? 0 : 10)
+      .opacity(isAnimation ? 1 : 0)
+      .scaleEffect(isAnimation ? 1 : 0.5)
+      .animation(.easeOut(duration: 1), value: isAnimation)
+      .onAppear(perform: {
+        isAnimation = true
+      })
     }
 }
 
